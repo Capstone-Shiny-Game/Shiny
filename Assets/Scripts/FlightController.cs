0e70159345002e39c77be706067a5b51baf25c80
Angelique Aban <angeliqueaban@gmail.com>
2021-10-21 02:50:14 -0600

    Flight Controller
    +Cleaned up code and added comments
    +player won't change speed if in a "straight enough" glide mode

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -10,27 +10,19 @@ using UnityEngine;
 public class FlightController : MonoBehaviour
 {
     public float forwardSpd = 15f, strafeSpd = 7.5f, hoverSpd = 5f;
-    private float currForwardSpd, currHoverSpd;
     float pitchAngle = .45f;
     float tiltAngle = 60f;
 
     public Camera cam;
     public float speed = 15.0f;
     public float brake = 0.0f;
-    // Start is called before the first frame update
+    public float glideThreshold = 0.1f;
 
-    public Transform start;
-    public Transform end;
-
-    // Time to move from sunrise to sunset position, in seconds.
-    public float journeyTime = 1.0f;
-    public float tiltTime = 2.0f;
+    // Time to move back from the tilted position, in seconds.
+    private float smoothTilt = 2.0f;
 
     // The time at which the animation started.
-    private float startTime = -1;
-    private float startTilt = -1;
-
-    private bool doUTurn = false;
+    private float start = -1;
     private bool hasTilted = false;
 
     private Vector3 velocity = Vector3.zero;
@@ -42,35 +34,25 @@ public class FlightController : MonoBehaviour
     }
     void LateUpdate()
     {
-        MoveCamera();
+        MoveCamera(); //moving the camera during update causes jitter
     }
 
     private void MoveCamera()
     {
-        // Vector3 moveCamTo = transform.position - transform.forward * 10.0f + Vector3.up * 3.0f;
+        //don't snap camera immediately to player
         float bias = 0.96f;
-        //cam.transform.position = cam.transform.position * bias + moveCamTo * (1.0f-bias);
-        // cam.transform.LookAt(transform.position + transform.forward*20.0f);
-
-        //Vector3 point = cam.WorldToViewportPoint(transform.position);
-        //Vector3 delta = transform.position - cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+        //Move the camera away if the player is faster
         Vector3 delta = transform.position - transform.forward * 22.0f + Vector3.up * 3.0f;
         Vector3 destination = cam.transform.position * bias + delta * (1.0f - bias);
 
         cam.transform.position = Vector3.SmoothDamp(cam.transform.position, destination, ref velocity, 0.01f);
-        //cam.transform.LookAt(transform.position + transform.forward * 20.0f);
-        //cam.transform.rotation = Quaternion.LookRotation(destination, transform.forward);
         Vector3 relativePos = transform.position - cam.transform.position;
-
-        // the second argument, upwards, defaults to Vector3.up
-        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up); //camera.LookAt will spin the camera if player is looking directly up or down
         cam.transform.rotation = rotation;
     }
-    private void Fly()
+    private void Brake()
     {
-        //set the acceleration based on if bird is pointed up or down
-        speed -= transform.forward.y * Time.deltaTime * 30.0f;
-        if (Input.GetKey(KeyCode.LeftControl) && speed > 5f)
+        if (Input.GetKey(KeyCode.LeftControl) && speed > 5f) //don't brake if speed negative
             brake += 0.01f;
         else
             brake -= 0.01f;
@@ -80,36 +62,55 @@ public class FlightController : MonoBehaviour
         else
             speed = Mathf.Clamp(speed, -5f, 100f);
         speed -= brake;
+    }
+    private void Fly()
+    {
+        //set the acceleration based on if bird is pointed up or down
+        //don't alter speed if relatively straight
+        if (Math.Abs(transform.forward.y) > glideThreshold) 
+            speed -= transform.forward.y * Time.deltaTime * 30.0f;
+        Brake();
         transform.position += transform.forward * Time.deltaTime * speed;         //once flying, the bird is always moving
 
-
+        
+        // Rotate
         float turn = Input.GetAxis("Horizontal") * tiltAngle / 1.5f * Time.deltaTime;
         float pitch = -Input.GetAxis("Vertical") * pitchAngle;
         float tilt = -Input.GetAxis("Horizontal") * tiltAngle * Time.deltaTime;
-
-        // Rotate
         transform.Rotate(new Vector3(pitch, turn, tilt));
 
+        ClampRotations();
+
+        if (tilt != 0)
+            hasTilted = true;
+        if (tilt == 0 && hasTilted) //if no longer turning, then reset the tilt to 0
+        {
+            if (start < 0)
+            {
+                start = Time.time;
+                smoothTilt = Math.Abs(Math.Min(transform.rotation.z, 360f - transform.rotation.z)) * 15f;
+            }
+            float fracComplete = (Time.time - start) / smoothTilt;
+            DampenAngleToZero("z", fracComplete);
+        }
+
+    }
+    private void ClampRotations()
+    {
         // Clamp Tilt
         float angle = transform.rotation.eulerAngles.z;
         float angleX = transform.rotation.eulerAngles.x;
         //tilted too far left
         if (angle >= 45f && angle < 180f)
         {
-            Debug.Log("ANGLE " + angle);
-
             float diff = angle - 45f;
             transform.Rotate(new Vector3(0, 0, -diff));
         }
         else if (angle < 315f && angle >= 180f)
         {
-            Debug.Log("ANGLE " + angle);
-
-            float diff = angle -315f;
+            float diff = angle - 315f;
             transform.Rotate(new Vector3(0, 0, -diff));
-
         }
-
         //tilted too far down
         if (angleX >= 80f && angleX < 180f)
         {
@@ -121,47 +122,27 @@ public class FlightController : MonoBehaviour
             float diff = angleX - 280f;
             transform.rotation *= Quaternion.AngleAxis(-diff, Vector3.right);
         }
-
-        if (tilt != 0)
-            hasTilted = true;
-        if (tilt == 0 && hasTilted) //if not pressing AD, then reset the tilt to 0
-        {
-            if (startTilt < 0)
-            {
-                startTilt = Time.time;
-                tiltTime = Math.Abs(360f - transform.rotation.z);
-            }
-            float fracComplete = (Time.time - startTilt) / tiltTime;
-            Quaternion target = DampenAngleToZero("z", fracComplete);
-
-            float diff = transform.rotation.eulerAngles.z - target.eulerAngles.z;
-            float degree = 1f;
-            if (Mathf.Abs(diff) <= degree)
-            {
-                startTilt = -1;
-                hasTilted = false;
-            }
-        }
-
     }
-
-    private Quaternion DampenAngleToZero(string angle, float fracComplete)
+    private void DampenAngleToZero(string angle, float fracComplete)
     {
-        Quaternion target = transform.rotation;
-        target.x /= target.w;
-        target.y /= target.w;
-        target.z /= target.w;
-        target.w = 1.0f;
 
-        float angleZero = 2.0f * Mathf.Rad2Deg * Mathf.Atan(0);
-        if(angle.ToLower() == "x")
-            target.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleZero);
-        else if(angle.ToLower() == "y")
-            target.y = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleZero);
-        else if(angle.ToLower() == "z")
-            target.z = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleZero);
+        Quaternion target = Quaternion.identity;
+
+        if (angle.ToLower() == "x")
+            target = Quaternion.Euler(0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        else if (angle.ToLower() == "y")
+            target = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
+        else if (angle.ToLower() == "z")
+            target = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
 
         transform.rotation = Quaternion.Slerp(transform.rotation, target, fracComplete);
-        return target;
+
+        float diff = transform.rotation.eulerAngles.z - target.eulerAngles.z;
+        float degree = 1f;
+        if (Mathf.Abs(diff) <= degree) //close enough to straight - reset damping
+        {
+            start = -1;
+            hasTilted = false;
+        }
     }
 }
