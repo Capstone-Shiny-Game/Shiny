ba5ecfb19382e367e5e4d892e6641a03b1827d98
Ella Moskun <ella.moskun@utah.edu>
2021-11-03 01:44:41 -0600

    Walking works!
    * Tag terrain
    * Only have one terrain
    * Update WalkingController to position crow above terrain
    
    #18
    #25

--- Assets/Scripts/Player/WalkingController.cs
+++ Assets/Scripts/Player/WalkingController.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class WalkingController : MonoBehaviour
 {
-    public float ForwardSpeed = 8f;
-    public float BackwardsSpeed = 4f;
-    public float TurningSpeed = 60f;
+    public float ForwardSpeed = 8;
+    public float BackwardsSpeed = 4;
+    public float TurningSpeed = 60;
+    public float HeightOffset = 0.5f;
 
     void Start()
     {
         GetComponent<CameraController>().isWalking = true;
         Vector3 v = transform.eulerAngles;
-        v.x = 0f;
+        v.x = 0;
         transform.eulerAngles = v;
     }
 
@@ -20,5 +21,8 @@ public class WalkingController : MonoBehaviour
         float displacement = Input.GetAxis("Vertical") * Time.deltaTime;
         displacement *= displacement >= 0 ? ForwardSpeed : BackwardsSpeed;
         transform.position += transform.forward * displacement;
+        Vector3 pos = transform.position;
+        pos.y = Terrain.activeTerrain.SampleHeight(pos) + HeightOffset;
+        transform.position = pos;
     }
 }
