cd1981e1e922498ec6c11340d6744be5585accb7
wksnders <42388072+wksnders@users.noreply.github.com>
2021-11-21 13:02:56 -0700

    pause menu methods for time stop

--- Assets/Scripts/PauseMenu/PauseMenu.cs
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
     {
-        
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Resume()
     {
-        
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+
     }
+
+    public void Home(int SceneID) {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneID);
+
+    }
+
 }
