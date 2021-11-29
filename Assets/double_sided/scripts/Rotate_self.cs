2fcc7f25beef67362a07ac47b4e8781d146a834e
ErPast <erika.prestwich@gmail.com>
2021-11-29 04:52:36 -0700

    Added new foliage and double sided shader mod

---
+++ Assets/double_sided/scripts/Rotate_self.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_self : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    float num = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        num += Time.deltaTime * 40;
+        if (num > 360)
+            num = 0;
+        this.transform.rotation = Quaternion.Euler(0, num, 0);
+    }
+}
