7b5b93ee400786b9f6a02369e947a37598d3ae67
Matt Mader <mcmader1@gmail.com>
2021-12-01 00:08:21 -0700

    Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject controlsMenu;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("CityLayoutTest");
+    }
+
+    public void enterControls()
+    {
+        mainMenu.SetActive(false);
+        controlsMenu.SetActive(true);
+    }
+
+    public void exitControls()
+    {
+        mainMenu.SetActive(true);
+        controlsMenu.SetActive(false);
+    }
+}
