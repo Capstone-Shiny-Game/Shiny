e9ce7b10ec4d9f967217a3fbfb2782d4257f97ad
Jesse <jesse.toftum@utah.edu>
2021-12-02 19:06:38 -0700

    making changes to UI_inventory.cs

--- Assets/Scripts/Inventory/UI_inventory.cs
+++ Assets/Scripts/Inventory/UI_inventory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class UI_inventory : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class UI_inventory : MonoBehaviour
     private Transform itemSlotContainer;
     private Transform itemSlotTemplate;
     public float itemSlotCellSize = 30f;
+    public float selectedItemSize = 60f;
+    public float nextItemSize = 30f;
     public int slotsPerRow = 4;
 
     private void Awake()
@@ -23,12 +26,12 @@ public class UI_inventory : MonoBehaviour
         inventory = inv;
 
         inventory.OnItemListChanged += Inventory_OnItemListChanged;
-        RefreshInventoryItems();
+        RefreshHotbarItems();
     }
 
     private void Inventory_OnItemListChanged(object sender, System.EventArgs e)
     {
-        RefreshInventoryItems();
+        RefreshHotbarItems();
     }
 
     private void UpdateUISelection()
@@ -38,27 +41,24 @@ public class UI_inventory : MonoBehaviour
 
     private void RefreshInventoryItems()
     {
-        //destory old ui elements to avoid duplicates
-        foreach (Transform child in itemSlotContainer)
-        {
-            if (child == itemSlotTemplate)
-            {//dont destory the template
-                continue;
-            }
-            Destroy(child.gameObject);
-        }
-        
+        return;
+    }
 
-        int x = 0;
-        int y = 0;
-        foreach (Item item in inventory.itemList)
+    private void RefreshHotbarItems()
+    {
+        ClearInventory();
+
+        for (int i = 0; i < Math.Min(inventory.itemList.Count, 2); i++)
         {
-            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();//instantiate template
+            Item item = inventory.itemList[i];
+
+            // instantiate template
+            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
             itemSlotRectTransform.gameObject.SetActive(true);
-            //find and set item image
+            // find and set item image
             Image image = itemSlotRectTransform.Find("itemImage").GetComponent<Image>();
             image.sprite = item.GetSprite();
-            //find and set text to display amount of items
+            // find and set text to display amount of items
             TextMeshProUGUI uiText = itemSlotRectTransform.Find("amountText").GetComponent<TextMeshProUGUI>();
             if (item.amount > 1)
             {
@@ -68,19 +68,74 @@ public class UI_inventory : MonoBehaviour
             {
                 uiText.SetText("");
             }
-            //item slots in grid array
-            itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, -y * itemSlotCellSize);
-            x++;//go to next position
-            //reached end of row, return to start
-            if (x >= slotsPerRow)
-            {
-                x = 0;
-                y++;
-            }
+            // item slots in grid array
+            itemSlotRectTransform.anchoredPosition = new Vector2(i * itemSlotCellSize, 0f);
+            // go to next position
 
         }
 
         UpdateUISelection();
 
+    }
+
+    // private void RefreshInventoryItems()
+    // {
+    //     ClearInventory();
+
+    //     int x = 0;
+    //     int y = 0;
+    //     for (int i = 0; i < inventory.itemList.Count; i++)
+    //     {
+    //         if (i >= 2)
+    //         {
+    //             break;
+    //         }
+    //         Item item = inventory.itemList[i];
+
+    //         // instantiate template
+    //         RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+    //         itemSlotRectTransform.gameObject.SetActive(true);
+    //         // find and set item image
+    //         Image image = itemSlotRectTransform.Find("itemImage").GetComponent<Image>();
+    //         image.sprite = item.GetSprite();
+    //         // find and set text to display amount of items
+    //         TextMeshProUGUI uiText = itemSlotRectTransform.Find("amountText").GetComponent<TextMeshProUGUI>();
+    //         if (item.amount > 1)
+    //         {
+    //             uiText.SetText(item.amount.ToString());
+    //         }
+    //         else
+    //         {
+    //             uiText.SetText("");
+    //         }
+    //         // item slots in grid array
+    //         itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, -y * itemSlotCellSize);
+    //         // go to next position
+    //         x++;
+    //         // reached end of row, return to start
+    //         if (x >= slotsPerRow)
+    //         {
+    //             x = 0;
+    //             y++;
+    //         }
+    //     }
+    //     UpdateUISelection();
+    // }
+
+    /// <summary>
+    /// Clears out all instantiated sprites
+    /// </summary>
+    private void ClearInventory()
+    {
+        // destroy old ui elements to avoid duplicates
+        foreach (Transform child in itemSlotContainer)
+        {
+            if (child == itemSlotTemplate)
+            {
+                // don't destroy the template
+                continue;
+            }
+            Destroy(child.gameObject);
+        }
     }
 }
