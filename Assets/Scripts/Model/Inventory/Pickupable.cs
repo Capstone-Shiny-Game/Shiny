a64db6a942816154b45976fc609c30f778c98741
Ella Moskun <ella.moskun@utah.edu>
2022-01-20 01:19:42 -0700

    Temporary Pickupable keyboard input
    #109

--- Assets/Scripts/Model/Inventory/Pickupable.cs
+++ Assets/Scripts/Model/Inventory/Pickupable.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 // TODO (Elaine) : We need to decide exactly how the pickup-but-not-add-to-inventory behavior should work.
 // TODO (Ella) : Replace this code with something more appropriate for long term use per above. #110
@@ -10,11 +11,22 @@ public class Pickupable : MonoBehaviour
     private new Rigidbody rigidbody;
     private PlayerController playerController;
     private FlightController flightController;
-
+    private InputAction grabAction = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/E"); // TODO : This seems very wrong. Angelique would know what to do better.
     private Vector3 smallScale;
     private Vector3 largeScale;
     private bool attached;
     private bool inRange;
+
+    private void OnEnable()
+    {
+        grabAction.performed += ctx => TryGrabOrRelease();
+        grabAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        grabAction.Disable();
+    }
 
     private void Start()
     {
@@ -27,19 +39,6 @@ public class Pickupable : MonoBehaviour
 
     private void Update()
     {
-        if (false) //Input.GetKeyDown(KeyCode.E))
-        {
-            if (attached)
-            {
-                // TODO : get speed when walking or splashing
-                float speed = playerController.state == PlayerController.CrowState.Flying ? flightController.speed : 0;
-                rigidbody.velocity = Crow.transform.forward * speed;
-                attached = false;
-            }
-            else if (inRange)
-                attached = true;
-        }
-
         rigidbody.useGravity = !attached;
 
         if (attached)
@@ -47,6 +46,19 @@ public class Pickupable : MonoBehaviour
 
         // TODO : we should probably do something more clever than just embiggening to indicate that the player needs to interact with this
         transform.localScale = inRange && !attached ? largeScale : smallScale;
+    }
+
+    private void TryGrabOrRelease()
+    {
+        if (attached)
+        {
+            // TODO : get speed when walking or splashing
+            float speed = playerController.state == PlayerController.CrowState.Flying ? flightController.speed : 0;
+            rigidbody.velocity = Crow.transform.forward * speed;
+            attached = false;
+        }
+        else if (inRange)
+            attached = true;
     }
 
     private void OnTriggerEnter(Collider other)
