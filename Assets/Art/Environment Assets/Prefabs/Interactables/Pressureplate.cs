9fc294689de38a635a0293fcb9bb36772d88779c
ErPast <erika.prestwich@gmail.com>
2022-01-23 00:35:09 -0700

    lerp

--- Assets/Art/Environment Assets/Prefabs/Interactables/Pressureplate.cs
+++ Assets/Art/Environment Assets/Prefabs/Interactables/Pressureplate.cs
@@ -7,32 +7,42 @@ public class Pressureplate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Pickupable"))
         {
-           
+
             GetComponent<Collider>().enabled = false;
-            StartCoroutine(moveDown());
+            StartCoroutine(moveDown(transform.position, new Vector3(transform.position.x, (transform.position.y - .7f), transform.position.z)));
         }
 
-        
+
     }
 
-    IEnumerator moveDown()
+    IEnumerator moveDown(Vector3 oldPos, Vector3 newPos)
     {
-        int move = 1;
-        while (move < 7)
-        {
-            transform.position = new Vector3(transform.position.x, (transform.position.y - .1f), transform.position.z);
-            move++ ;
-            yield return new WaitForSeconds(.5f);
-        }
-    }
-    
+        //  int move = 1;
+        // while (move < 7)
+        // {
+        //    transform.position = new Vector3(transform.position.x, (transform.position.y - .1f), transform.position.z);
+        //     move++ ;
+        //     yield return new WaitForSeconds(.5f);
+        //}
 
- 
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime / 3;
+            transform.position = Vector3.Lerp(oldPos, newPos, t);
+
+            yield return null;
+        }
+
+    }
+
+
+
 }
