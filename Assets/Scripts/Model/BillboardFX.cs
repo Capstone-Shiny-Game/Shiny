f0ba8a1e6b5fea4554025b6a02b5b1a04f1cb498
Matt <mcmader1@gmail.com>
2022-01-23 15:22:30 -0700

    Distance Check for BillboardFX

--- Assets/Scripts/Model/BillboardFX.cs
+++ Assets/Scripts/Model/BillboardFX.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class BillboardFX : MonoBehaviour
 {
+    public float VanishDistance = 100;
     void Update()
     {
-        transform.LookAt(transform.position + Camera.main.transform.rotation * -Vector3.back,
-            Camera.main.transform.rotation * -Vector3.down);
+        if(Vector3.Distance(transform.position, Camera.main.transform.position) < VanishDistance)
+        {
+            gameObject.GetComponent<Canvas>().enabled = true;
+            transform.LookAt(transform.position + Camera.main.transform.rotation * -Vector3.back, Camera.main.transform.rotation * -Vector3.down);
+        }
+        else
+        {
+            gameObject.GetComponent<Canvas>().enabled = false;
+        }
+
     }
 }
