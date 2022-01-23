0e2ed2e230b5c0f2eeed56b9d2186b05e7660c2e
ErPast <erika.prestwich@gmail.com>
2022-01-23 15:31:57 -0700

    dirt_mound

--- Assets/Art/Environment Assets/Prefabs/Interactables/Dig_controller.cs
+++ Assets/Art/Environment Assets/Prefabs/Interactables/Dig_controller.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Dig_controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Animator animator;
+
+
+    //when the button is pressed 
+    // animator.SetBool("Dig", true);
+
+    //then spawn public pickupable obj
+    //
+
+    //then fade out and delete
 
     // Update is called once per frame
     void Update()
