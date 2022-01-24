663f3c3448605b472d9fe5fd77cc89dabe03276f
Jesse <jesse.toftum@utah.edu>
2022-01-23 17:17:17 -0700

    Added dig animation hijinks

--- Assets/Art/Environment Assets/Prefabs/Interactables/Dig_controller.cs
+++ Assets/Art/Environment Assets/Prefabs/Interactables/Dig_controller.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class Dig_controller : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private Vector3 spawnLocation = Vector3.zero;
 
 
-    //when the button is pressed 
-    // animator.SetBool("Dig", true);
-
-    //then spawn public pickupable obj
-    //
-
-    //then fade out and delete
-
-    // Update is called once per frame
-    void Update()
+    // when the button is pressed
+    public void OnButtonInteraction()
     {
-        
+        animator.SetTrigger("Dig");
+        Destroy(GetComponentInChildren<Canvas>().gameObject);
+        var sphere = GetComponentInChildren<MeshRenderer>().gameObject;
+    }
+
+    /// <summary>
+    /// Okay. So. Unity is kinda dumb and doesn't have a way of setting
+    /// callbacks for when animations are done playing programmatically.
+    /// Instead, the workaround used here involves going into the animator
+    /// object (the same one passed to this class), and clicking "Add
+    /// behaviour". From here, a script is added which extends
+    /// StateMachineBehaviour. There's a method called `OnStateExit` which can
+    /// be overridden to essentially use as a callback. In there, you can define
+    /// a reference to a class (like this one) and call a method within that class.
+    /// For an example, see `DigFinishPlaying.cs`
+    /// </summary>
+    public void spawnObject()
+    {
+        var sphere = GetComponentInChildren<MeshRenderer>().gameObject;
+        Destroy(sphere);
+        Instantiate(objectToSpawn, transform.position + spawnLocation, transform.rotation);
     }
 }
