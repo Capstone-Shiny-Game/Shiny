44fbe7207ad1bc8c833914ebc26e19b35305022c
ErPast <erika.prestwich@gmail.com>
2022-02-03 18:48:29 -0700

    added particles system

--- Assets/Art/Environment Assets/Prefabs/Interactables/Balloon_control.cs
+++ Assets/Art/Environment Assets/Prefabs/Interactables/Balloon_control.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Balloon_col : MonoBehaviour
+public class Balloon_control : MonoBehaviour
 {
     [SerializeField] private Animator animator;
 
