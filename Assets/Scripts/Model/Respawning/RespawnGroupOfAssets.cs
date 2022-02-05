01bc98415a5de7e61e7c169aa9acda87a31d9211
Jesse <jesse.toftum@utah.edu>
2022-02-05 15:16:02 -0700

    Add start to RespawnGroupOfAssets.cs

--- Assets/Scripts/Model/Respawning/RespawnGroupOfAssets.cs
+++ Assets/Scripts/Model/Respawning/RespawnGroupOfAssets.cs
@@ -1,19 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RespawnGroupOfAssets : MonoBehaviour
 {
-    //TODO deal with pop-in issues
-    public float respawnTime;
-    public List<Respawnable> respawnable; //the child objects to respawn
+    // TODO: deal with pop-in issues
+    private GameObject playerReference;
+    public float respawnTime = 60;
+    public float minDistanceToRespawn = 100;
+    [Tooltip("The maximum number of items to spawn. 0 means as many as possible (no more than the number of children).")]
+    public int maxAmountToSpawn = 0;
+    // the child objects to respawn
+    private List<bool> spawnedPrefabs;
+    private List<int> availableToSpawn;
+    private List<GameObject> prefabs;
     void Start()
     {
-        //respawnable.RespawnME.AddListener(StartRespawn);
+        playerReference = GameObject.FindGameObjectWithTag("Player");
+        prefabs = new List<GameObject>(gameObject.GetComponentsInChildren<GameObject>());
+        if (maxAmountToSpawn <= 0)
+        {
+            maxAmountToSpawn = prefabs.Count;
+        }
+        for (int i = 0; i < maxAmountToSpawn; i++)
+        {
+            spawnedPrefabs[i] = false;
+            availableToSpawn[i] = i;
+            prefabs[i].SetActive(false);
+        }
+        for (int i = 0; i < maxAmountToSpawn; i++)
+        {
+            int prefabIndex = Random.Range(0, maxAmountToSpawn);
+            int availableIndex = Random.Range(0, availableToSpawn.Count);
+            GameObject prefabLocation = prefabs[availableToSpawn[availableIndex]];
+            Transform placeToSpawn = prefabLocation.GetComponent<Transform>();
+            GameObject newObject = Instantiate(prefabs[prefabIndex], placeToSpawn);
+            newObject.SetActive(true);
+            Respawnable script = newObject.AddComponent<Respawnable>();
+            script.RespawnME = new UnityEvent();
+            script.RespawnME.AddListener(StartRespawn);
+            script.index = availableIndex;
+        }
+
+        // respawnable.RespawnME.AddListener(StartRespawn);
     }
+
     void StartRespawn()
     {
-        if (this.gameObject.activeInHierarchy)//prevents exception when game is closed
+        // prevents exception when game is closed
+        if (this.gameObject.activeInHierarchy)
         {
             StartCoroutine("Respawn");
         }
@@ -21,9 +57,10 @@ public class RespawnGroupOfAssets : MonoBehaviour
     public IEnumerator Respawn()
     {
         yield return new WaitForSeconds(respawnTime);
-        if (this.gameObject.activeInHierarchy)//prevents exception when game is closed
+        // prevents exception when game is closed
+        if (this.gameObject.activeInHierarchy)
         {
-            //respawnable.gameObject.SetActive(true);
+            // respawnable.gameObject.SetActive(true);
         }
     }
 }
--- Assets/Scripts/Model/Respawning/Respawnable.cs
+++ Assets/Scripts/Model/Respawning/Respawnable.cs
@@ -11,18 +11,23 @@ using UnityEngine.Events;
 public class Respawnable : MonoBehaviour
 {
     [field: HideInInspector] public UnityEvent RespawnME;
-    private void Start()
-    {
-        if (RespawnME == null)
-            RespawnME = new UnityEvent();
+    [field: HideInInspector] public int index;
 
-    }
     /// <summary>
     /// when this object is no longer active, let the parent know so that it can handle the reactivation.
-    /// this is done because monobehaviors are disabled when the gameobject is deactivated, thus it can't reactivate its self.
+    /// this is done because monobehaviors are disabled when the gameobject is deactivated, thus it can't reactivate itself.
     /// </summary>
     private void OnDisable()
     {
-        RespawnME.Invoke();
+        RespawnME?.Invoke();
+    }
+
+    /// <summary>
+    /// when this object is destroyed, let the parent know so that it can handle the reactivation.
+    /// this is done because monobehaviors are disabled when the gameobject is deactivated, thus it can't reactivate itself.
+    /// </summary>
+    private void OnDestroy()
+    {
+        RespawnME?.Invoke();
     }
 }
