dae9d556c90f366e006ed4e05611e378bc636ffb
Ella Moskun <ella.moskun@utah.edu>
2022-02-13 00:14:36 -0700

    Add/move logic to TransformExtensions

--- Assets/Scripts/Model/GroundDetector.cs
+++ Assets/Scripts/Model/GroundDetector.cs
@@ -1,25 +1,11 @@
+using System;
 using UnityEngine;
 
 public class GroundDetector : MonoBehaviour
 {
-    private const float RaycastDistance = 100;
-
+    [Obsolete]
     public bool FindGround(out Vector3 groundPos, out bool isWater)
     {
-        groundPos = Vector3.zero;
-        isWater = false;
-
-        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, RaycastDistance)
-            && (hit.transform.tag == "Terrain" || hit.transform.tag == "Water"))
-        {
-           // Debug.Log("HIT: " + hit.transform.tag);
-            groundPos = hit.point + new Vector3(0, transform.localScale.y / 2, 0);
-            isWater = hit.transform.tag == "Water";
-            return true;
-        }
-        else
-            return false;
+        return transform.CastGround(out groundPos, out isWater, transform.localScale.y / 2);
     }
-
-    // TODO (Ella #66) : Consider normal
 }
--- Assets/Scripts/Model/TransformExtensions.cs
+++ Assets/Scripts/Model/TransformExtensions.cs
@@ -2,13 +2,6 @@ using UnityEngine;
 
 public static class TransformExtensions
 {
-    public static Vector3 FindGround(this Transform transform, float offset = 0)
-    {
-        Vector3 ground = transform.position;
-        ground.y = transform.NearestTerrain().SampleHeight(transform.position) + offset;
-        return ground;
-    }
-
     public static Terrain NearestTerrain(this Transform transform)
     {
         Terrain nearest = null;
@@ -24,5 +17,54 @@ public static class TransformExtensions
             }
         }
         return nearest;
+    }
+
+    public static Vector3 FindGround(this Transform transform, float offset = 0)
+    {
+        Vector3 ground = transform.position;
+        ground.y = transform.NearestTerrain().SampleHeight(transform.position) + offset;
+        return ground;
+    }
+
+    public static bool CastGround(this Transform transform, out Vector3 ground, out bool water, float offset)
+    {
+        ground = Vector3.zero;
+        water = false;
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, transform.position.y + 200))
+        {
+            if (hit.collider is TerrainCollider || hit.collider.CompareTag("Terrain"))
+            {
+                ground = hit.point;
+                ground.y += offset;
+                return true;
+            }
+            else if (hit.collider.CompareTag("Water"))
+            {
+                water = true;
+                ground = hit.point;
+                ground.y += offset;
+                return true;
+            }
+         }
+        return false;
+    }
+
+    public static void TestCollision(this Transform transform, Vector3 position, out bool collided, out bool raycastNeeded)
+    {
+        Collider[] colliders = Physics.OverlapSphere(position, transform.localScale.magnitude);
+        collided = false;
+        raycastNeeded = false;
+        foreach (Collider collider in colliders)
+        {
+            if (collider.isTrigger || collider is TerrainCollider || collider.CompareTag("Player"))
+                continue;
+            else if (collider.CompareTag("Terrain") || collider.CompareTag("Water"))
+                raycastNeeded = true;
+            else
+            {
+                collided = true;
+                break;
+            }
+        }
     }
 }
