01775522c2b96a460881445b9ef4ac366af5aa91
Jakob <horvath.j17@gmail.com>
2022-02-21 19:40:59 -0700

    #152 can plant seeds

--- Assets/Scripts/Model/FarmPlot.cs
+++ Assets/Scripts/Model/FarmPlot.cs
@@ -10,6 +10,7 @@ public class FarmPlot : MonoBehaviour
 
     private GameObject s1, s2, s3;
     private int currMesh;
+    private bool hasCrop;
 
 
     private void Start()
@@ -18,9 +19,10 @@ public class FarmPlot : MonoBehaviour
         s2 = transform.Find("stage2").GetChild(0).gameObject;
         s3 = transform.Find("stage3").GetChild(0).gameObject;
         currMesh = -1;
+        hasCrop = false;
 
         // TEST
-        TryInstantiatePlot("Acorn_Prefab");
+        //TryInstantiatePlot("Acorn_Prefab");
     }
 
     private void OnEnable()
@@ -37,11 +39,16 @@ public class FarmPlot : MonoBehaviour
     {
         if (other.CompareTag("Tradeable"))
         {
-            TryInstantiatePlot(other.name);
+            TryInstantiatePlot(other.name, out bool plantedCrop);
+            if (plantedCrop)
+            {
+                hasCrop = true;
+                Destroy(other.gameObject);
+            }
         }
     }
 
-    private void TryInstantiatePlot(string objName)
+    private void TryInstantiatePlot(string objName, out bool success)
     {
         List<string> meshNames = new List<string>();
         bool isProperItem = false;
@@ -61,22 +68,23 @@ public class FarmPlot : MonoBehaviour
         {
             s1.GetComponent<MeshFilter>().sharedMesh = Resources.Load<Mesh>(meshNames[0]);
             s1.SetActive(false);
-
             s2.GetComponent<MeshFilter>().sharedMesh = Resources.Load<Mesh>(meshNames[1]);
             s2.SetActive(false);
-
             s3.GetComponent<MeshFilter>().sharedMesh = Resources.Load<Mesh>(meshNames[2]);
             s3.SetActive(false);
+
+            success = true;
         }
         else
         {
             Debug.Log("OnTriggerEnter(): You've tried to plant an unexpected item");
+            success = false;
         }
     }
 
     private void LoadNextMesh()
     {
-        if (currMesh < 2)
+        if (hasCrop && currMesh < 2)
         {
             switch (++currMesh)
             {
