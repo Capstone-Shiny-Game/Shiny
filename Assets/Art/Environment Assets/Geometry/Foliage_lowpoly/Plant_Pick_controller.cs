6efc0443bab79b0101a4809552ffc424d4fa99dc
ErPast <36975707+ErPast@users.noreply.github.com>
2022-02-26 01:02:45 -0700

    Pickable plants

---
+++ Assets/Art/Environment Assets/Geometry/Foliage_lowpoly/Plant_Pick_controller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant_Pick_controller : MonoBehaviour
+{
+
+    private void Start()
+    {
+        gameObject.GetComponentInChildren<Rigidbody>().useGravity = false;
+        gameObject.GetComponentInChildren<SphereCollider>().enabled = false;
+        gameObject.GetComponentInChildren<CapsuleCollider>().enabled = false;
+
+    }
+    public void OnPick()
+    {
+        
+        gameObject.GetComponentInChildren<SphereCollider>().enabled = true;
+        gameObject.GetComponentInChildren<CapsuleCollider>().enabled = true;
+        gameObject.GetComponentInChildren<Rigidbody>().AddForce(0,15,0);
+        gameObject.GetComponentInChildren<Rigidbody>().useGravity = true;
+        
+        
+        Destroy(gameObject.GetComponentInChildren<Canvas>().gameObject);
+    }
+
+
+
+}
