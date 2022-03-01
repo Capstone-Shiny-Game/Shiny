4a31e37fe77e497f1633ee8c81df7fbbef3d154f
Ella Moskun <ella.moskun@utah.edu>
2022-03-01 15:09:35 -0700

    remove example types

--- Assets/QuestSystem/QSTypes.cs
+++ Assets/QuestSystem/QSTypes.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
 
-public static class QS
+public static partial class QS
 {
     public static readonly List<(string, Type)> NodeTypes = new List<(string, Type)>() {
-        ("Quest/Super Amazing Quest Type", typeof(DeleteThisQSExampleQuestNodeSO)),
-        ("Action/Awesome Quest Helper Action", typeof (DeleteThisQSExampleActionNodeSO)),
         ("Utility/Start", typeof (QSStartNodeSO)),
         ("Utility/And", typeof (QSAndNodeSO)),
         ("Utility/Or", typeof (QSOrNodeSO)),
