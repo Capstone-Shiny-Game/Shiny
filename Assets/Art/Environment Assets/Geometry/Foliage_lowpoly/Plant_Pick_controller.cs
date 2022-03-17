f04eda40956eb303fce18528f2e33e517ea0f4bf
ErPast <erika.prestwich@gmail.com>
2022-03-17 04:25:01 -0600

    fixed sinking plants bug

--- Assets/Art/Environment Assets/Geometry/Foliage_lowpoly/Plant_Pick_controller.cs
+++ Assets/Art/Environment Assets/Geometry/Foliage_lowpoly/Plant_Pick_controller.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class Plant_Pick_controller : MonoBehaviour
 {
+    [SerializeField] private GameObject objectToSpawn;
 
     private void Start()
     {
-        gameObject.GetComponentInChildren<Rigidbody>().useGravity = false;
-        gameObject.GetComponentInChildren<SphereCollider>().enabled = false;
-        gameObject.GetComponentInChildren<BoxCollider>().enabled = false;
+       // gameObject.GetComponentInChildren<Rigidbody>().useGravity = false;
+      //  gameObject.GetComponentInChildren<SphereCollider>().enabled = false;
+      //  gameObject.GetComponentInChildren<BoxCollider>().enabled = false;
 
     }
     public void OnPick()
     {
         
-        gameObject.GetComponentInChildren<SphereCollider>().enabled = true;
-        gameObject.GetComponentInChildren<BoxCollider>().enabled = true;
-        gameObject.GetComponentInChildren<Rigidbody>().AddForce(0,15,0);
-        gameObject.GetComponentInChildren<Rigidbody>().useGravity = true;
+       // gameObject.GetComponentInChildren<SphereCollider>().enabled = true;
+      //  gameObject.GetComponentInChildren<BoxCollider>().enabled = true;
+      //  gameObject.GetComponentInChildren<Rigidbody>().AddForce(0,15,0);
+      //  gameObject.GetComponentInChildren<Rigidbody>().useGravity = true;
         
         
         Destroy(gameObject.GetComponentInChildren<Canvas>().gameObject);
-        gameObject.transform.DetachChildren();
+        Destroy(gameObject.GetComponentInChildren<MeshRenderer>().gameObject);
+        
+        Instantiate(objectToSpawn, transform.position, transform.rotation);
+        //gameObject.transform.DetachChildren();
         Destroy(gameObject);
     }
 
