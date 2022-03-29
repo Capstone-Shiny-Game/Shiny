f8913699379fa3dc385decf175a9b1dce6cd623b
Ella Moskun <ella.moskun@utah.edu>
2022-03-28 21:25:37 -0600

    prototype : screen space buttons

--- Assets/Scripts/Model/BillboardFX.cs
+++ Assets/Scripts/Model/BillboardFX.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using UnityEngine.UI;
 
 public class BillboardFX : MonoBehaviour
 {
+
+
+
+
+    // HERE BE DRAGONS
+    private GameObject screenSpaceButton;
+
+
+
+
     public float VanishDistance = 100;
     void Update()
     {
@@ -11,10 +23,50 @@ public class BillboardFX : MonoBehaviour
         {
             gameObject.GetComponent<Canvas>().enabled = true;
             transform.LookAt(transform.position + Camera.main.transform.rotation * -Vector3.back, Camera.main.transform.rotation * -Vector3.down);
+
+
+
+
+            // HERE BE DRAGONS
+            if (gameObject.name.Contains("Interact") && gameObject.transform.parent.gameObject.name.Contains("Tulip"))
+            {
+                if (screenSpaceButton != null)
+                {
+                    Destroy(screenSpaceButton);
+                }
+
+
+                GameObject canvas = GameObject.Find("CanvasControls");
+                Vector2 local = RectTransformUtility.WorldToScreenPoint(Camera.main, transform.position);
+                local *= canvas.GetComponent<CanvasScaler>().scaleFactor;
+                screenSpaceButton = Instantiate(gameObject, local, Quaternion.identity, canvas.transform);
+                    screenSpaceButton.transform.SetParent(canvas.transform, false);
+                    screenSpaceButton.transform.position = local;
+                    screenSpaceButton.transform.localScale = Vector3.one * 0.1f;
+                    screenSpaceButton.transform.localRotation = Quaternion.identity;
+            }
+
+
+
+
         }
         else
         {
             gameObject.GetComponent<Canvas>().enabled = false;
+
+
+
+
+            // HERE BE DRAGONS
+            if (screenSpaceButton != null)
+            {
+                Destroy(screenSpaceButton);
+                screenSpaceButton = null;
+            }
+
+
+
+
         }
 
     }
