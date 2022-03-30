af2ab502aadc03f842a85abd3bbb490e99284db7
Matt <mcmader1@gmail.com>
2022-03-30 01:48:17 -0600

    I think I goofed with audio earlier

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CAMPFIRE = 1931646578U;
+        static const AkUniqueID DAYSTART = 3232797379U;
+        static const AkUniqueID DIGDIRTPILE = 3965959576U;
+        static const AkUniqueID FLYTHROUGHTREE = 1561107991U;
+        static const AkUniqueID LEVELSTART = 3372421815U;
+        static const AkUniqueID MENUSTART = 1447920996U;
+        static const AkUniqueID NIGHTSTART = 2986941311U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace TIMEOFDAY
+        {
+            static const AkUniqueID GROUP = 3729505769U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DAY = 311764537U;
+                static const AkUniqueID NIGHT = 1011622525U;
+            } // namespace SWITCH
+        } // namespace TIMEOFDAY
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TEMPBANK = 3633468881U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
