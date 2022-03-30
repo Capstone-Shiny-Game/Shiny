63276d6d44ae8c86e421a4d5488f0f99178b5e53
ErPast <erika.prestwich@gmail.com>
2022-03-29 20:35:29 -0600

    prefabs stuff

--- Assets/Art/Environment Assets/Prefabs/Interactables/Interactable_Scripts/Tree_spawn_control.cs
+++ Assets/Art/Environment Assets/Prefabs/Interactables/Interactable_Scripts/Tree_spawn_control.cs
@@ -6,17 +6,23 @@ public class Tree_spawn_control : MonoBehaviour
 {
 
 
-
+    [SerializeField] private GameObject objectToSpawn;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            
-            GetComponentInChildren<Rigidbody>().useGravity = true;
-            GetComponentInChildren<ParticleSystem>().Play();
-            GetComponent<SphereCollider>().enabled = false;
-            gameObject.transform.DetachChildren();
+
+            /*GetComponentInChildren<Rigidbody>().useGravity = true;*/
+
+            /* GetComponent<SphereCollider>().enabled = false;
+             //*/
             //
+            Destroy(gameObject.GetComponentInChildren<MeshRenderer>().gameObject);
+
+            Instantiate(objectToSpawn, transform.position, transform.rotation);
+
+            GetComponentInChildren<ParticleSystem>().Play();
+            gameObject.transform.DetachChildren();
             Destroy(gameObject);
         }
 
