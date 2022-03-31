309a625cf5a9943c2cae0be90a6911fe3b9a0ef6
Jakob <horvath.j17@gmail.com>
2022-03-31 15:43:06 -0600

    add particle effects to tt rings

--- Assets/Scripts/View/TimeTrialRing.cs
+++ Assets/Scripts/View/TimeTrialRing.cs
@@ -7,6 +7,18 @@ public class TimeTrialRing : MonoBehaviour
     public delegate void RingCollision(GameObject ring);
     public static event RingCollision OnRingCollision;
 
+    private ParticleSystem popinParticles;
+
+    private void Start()
+    {
+        popinParticles = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void OnEnable()
+    {
+        popinParticles.Play();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         OnRingCollision?.Invoke(gameObject);
