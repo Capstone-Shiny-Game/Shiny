99025152d531e8f0ae215dc0e6660bf6790467e7
Ella Moskun <ella.moskun@utah.edu>
2022-04-05 12:46:32 -0600

    small changes to OldestActiveQuest

--- Assets/Scripts/Model/QuestManager.cs
+++ Assets/Scripts/Model/QuestManager.cs
@@ -3,18 +3,38 @@
 
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+
+#if VERBOSE
 using UnityEngine;
+#endif
 
 public static class QuestManager
 {
-    private static List<string> active = new List<string>();
-    private static List<string> completed = new List<string>();
+    private static readonly List<string> active = new List<string>();
+    private static readonly List<string> completed = new List<string>();
 
     public static IEnumerable<string> ActiveQuests => active;
 
     public static IEnumerable<string> CompletedQuests => completed;
 
-    public static string OldestActiveQuest => active.Count > 0 ? active[0] : "Have Fun!";
+    public static string OldestActiveQuest
+    {
+        get
+        {
+            if (active.Count > 0)
+            {
+                return active[0];
+            }
+            else if (completed.Count > 0)
+            {
+                return "Have Fun!";
+            }
+            else
+            {
+                return "Explore!";
+            }
+        }
+    }
 
     public static void StartQuest(string quest)
     {
