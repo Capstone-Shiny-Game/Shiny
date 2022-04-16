970550d4f8b3d93a27e509c82488280e5fcb13eb
Jesse <jesse.toftum@utah.edu>
2022-04-15 23:38:06 -0600

    Try number 3 at getting animation to work woooooo

--- Assets/Scripts/View/UI/MenuManager.cs
+++ Assets/Scripts/View/UI/MenuManager.cs
@@ -29,6 +29,7 @@ public class MenuManager : MonoBehaviour
     [HideInInspector]
     public MenuType lastOpenedPauseMenu;
     public LevelLoader loader;
+    private bool isPaused;
 
     // Start is called before the first frame update
     void Start()
@@ -51,19 +52,26 @@ public class MenuManager : MonoBehaviour
     /// <param name="calledByConfirm">if it is called from the confirm popUP</param>
     public void SwitchMenu(MenuType menuType, bool calledByConfirm = false)
     {
-        //TODO fix bug with changing from save menu to load menu
+        // TODO fix bug with changing from save menu to load menu
         // Set Timescale and Scene
         switch (menuType)
         {
             // leaving pause menu and returning to normal time
             case MenuType.flightui:
                 DisablePause();
-                if (currentMenu.menuType != MenuType.flightui) //catch edge cases
-                {
-                    lastOpenedPauseMenu = currentMenu.menuType;
-                }
+                // We previously used the approach of opening the last opened menu, *however*
+                // we added animation to the book menu and it's just a heckin' struggle to
+                // try to get the previous opened menu to re-open, so I'm not gonna and you
+                // can't make me lol (but really if you have an issue just ping me or something,
+                // maybe you can think of a better idea than my smoothed-to-a-polish brain)
+
+                // if (currentMenu.menuType != MenuType.flightui)
+                // {
+                //     lastOpenedPauseMenu = currentMenu.menuType;
+                // }
                 break;
-            case MenuType.mainMenu://back to main menu reset to defaults
+            // back to main menu reset to defaults
+            case MenuType.mainMenu:
                 DisablePause();
                 StartCoroutine(loader.LoadLevel("MainMenu"));
                 currentMenu = menuContainers.Find(x => x.menuType == MenuType.flightui);
@@ -72,14 +80,15 @@ public class MenuManager : MonoBehaviour
                 EnablePause();
                 break;
         }
-        //replace current Menu
+        // replace current Menu
         if (!calledByConfirm && currentMenu.DisableSelf(menuType) == MenuType.wait)
-        { //return value is used to interrupt changing menus if disable self needs to override
+        {
+            // return value is used to interrupt changing menus if disable self needs to override
             return;
         }
         currentMenu = FindNextMenu(menuType);
         currentMenu.gameObject.SetActive(true);
-        //call setup on new menu
+        // call setup on new menu
         currentMenu.AfterEnableSetup(menuType);
     }
     private MenuContainer FindNextMenu(MenuType menuType)
@@ -110,6 +119,7 @@ public class MenuManager : MonoBehaviour
         // Should only have the one crow, and crow should only have the one
         // inputcontroller cs script attached
         crow.GetComponentInChildren<InputController>().menuOpen = false;
+        isPaused = false;
     }
 
     private void EnablePause()
@@ -118,7 +128,11 @@ public class MenuManager : MonoBehaviour
         onAllPauseMenus.SetActive(true);
         pauseMenuBackground.SetActive(true);
         Animator bookAnimator = pauseMenuBackground.GetComponentInChildren<Animator>();
-        bookAnimator.SetTrigger("Open");
+        if (!isPaused)
+        {
+            bookAnimator.SetTrigger("Open");
+            isPaused = true;
+        }
 
         // Disable camera controls when pause is enabled
         GameObject crow = GameObject.FindGameObjectWithTag("Player");
