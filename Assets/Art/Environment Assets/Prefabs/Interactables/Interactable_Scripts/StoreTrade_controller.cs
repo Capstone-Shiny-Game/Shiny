6e588031ced5802d9083b3b1360e1432b8bc40eb
ErPast <36975707+ErPast@users.noreply.github.com>
2022-04-15 21:49:45 -0600

    new trade controllder

---
+++ Assets/Art/Environment Assets/Prefabs/Interactables/Interactable_Scripts/StoreTrade_controller.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreTrade_controller : MonoBehaviour
+{
+    public GameObject[] acceptedPaymentPrefabs;
+    [SerializeField] private GameObject forSale;
+    [SerializeField] private GameObject spawnLocation;
+
+    private bool wait = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Tradeable") && checkAcceptedPayment(other.gameObject) && !wait)
+        {
+
+            wait = true;
+            
+            Instantiate(forSale, (other.transform.position + new Vector3(3, 3, 3)), Quaternion.identity);
+            Destroy(other.gameObject);
+            StartCoroutine(WaitTrade());
+        }
+
+    }
+
+    private bool checkAcceptedPayment(GameObject depositedObject)
+    {
+        foreach (GameObject obj in acceptedPaymentPrefabs)
+        {
+            if (depositedObject.name.Contains(obj.name))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    IEnumerator WaitTrade()
+    {
+        yield return new WaitForSeconds(1);
+        wait = false;
+    }
+}
