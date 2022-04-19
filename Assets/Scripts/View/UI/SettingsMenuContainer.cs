710600802656f31524e6c664a5a0df75a48d4b62
Matt <mcmader1@gmail.com>
2022-04-19 01:52:39 -0600

    capitalization error oops

--- Assets/Scripts/View/UI/SettingsMenuContainer.cs
+++ Assets/Scripts/View/UI/SettingsMenuContainer.cs
@@ -96,8 +96,8 @@ public class SettingsMenuContainer : MenuContainer
             preferredSettings = Settings.settingsData;
         }
         Settings.UpdateSettingData(preferredSettings);
-        AKSoundEngine.SetRTPCValue("MasterVolume", Settings.settingsData.dialogueVolume);
-        AKSoundEngine.SetRTPCValue("MusicVolume", Settings.settingsData.musicVolume);
+        AkSoundEngine.SetRTPCValue("MasterVolume", Settings.settingsData.dialogueVolume);
+        AkSoundEngine.SetRTPCValue("MusicVolume", Settings.settingsData.musicVolume);
         MenuManager.instance.SwitchMenu(nextMenuType,true);
     }
 
